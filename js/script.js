0aad10dd9413bf765a97ae11abaedf08d1956ee5
Magdalena <magdalenamalek52@gmail.comgit statusgit status git config --global user.email magdalenamalek52@gmail.com>
2022-04-12 17:40:28 +0200

    add generateTitleLinks function and remove bug

--- js/script.js
+++ js/script.js
@@ -29,9 +29,47 @@ const titleClickHandler = function(event){
     /* add class 'active' to the correct article */
     targetArticle.classList.add('active');
   }
+
+{ 
+  const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+    
+
+  function generateTitleLinks(){
+
+    /* remove contents of titleList */
+    titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+    
+    /* find all the articles and save them to variable: articles */
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    let html = '';
+
+    /* for each article */
+    for(let article of articles){
+      /* get the article id */
+      const articleId = article.getAttribute('id');
+      /* find the title element and get the title from the title element */
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+      /* create HTML of the link */
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      /* insert link into titleList */
+      html = html + linkHTML;
+    }
+    
+    titleList.innerHTML = html;
+
+    const links = document.querySelectorAll('.titles a');
   
-  const links = document.querySelectorAll('.titles a');
-  
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
+    for(let link of links){
+      link.addEventListener('click', titleClickHandler);
+    }
   }
+
+  generateTitleLinks();
+  
+}
+
+  
